10c738ab93371447d9a0d92691f3cad23b1ccf7c
ayumu-1212 <ayumuabe1434@gmail.com>
2024-06-23 15:55:05 +0900

    add: cargo build --release

---
+++ projects/hello_cargo/target/release/deps/hello_cargo-a9a69c254d8b486e.d
@@ -0,0 +1,5 @@
+/usr/src/projects/hello_cargo/target/release/deps/hello_cargo-a9a69c254d8b486e: src/main.rs
+
+/usr/src/projects/hello_cargo/target/release/deps/hello_cargo-a9a69c254d8b486e.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/usr/src/projects/hello_cargo/target/release/hello_cargo: /usr/src/projects/hello_cargo/src/main.rs
