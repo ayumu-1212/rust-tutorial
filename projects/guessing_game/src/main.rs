63303390825184937342df731bf6aaf80dbc69bb
ayumu-1212 <ayumuabe1434@gmail.com>
2024-06-26 13:04:51 +0000

    fix: Comment out debug print statement in guessing game loop

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
     loop {
         println!("Please input your guess.");
@@ -25,7 +25,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
