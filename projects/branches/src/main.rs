b4cd2c7397219da93dc6371cf7234a83800e07dc
ayumu-1212 <ayumuabe1434@gmail.com>
2024-06-29 05:11:48 +0000

    add: while

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,20 +1,11 @@
 fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
+    let mut number = 3;
 
-        count += 1;
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
     }
-    println!("end count = {count}");
+
+    println!("LIFTOFF!!!!");
 }
