dcb1c85856c03234769a60c4f260f124f960a5ba
ayumu-1212 <ayumuabe1434@gmail.com>
2024-06-30 04:14:26 +0000

    add: sample

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s = String::from("hello");
 
-    change(&mut s);
-}
+    let r1 = &s;
+    let r2 = &s;
 
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    println!("{r1}, {r2}");
 }
