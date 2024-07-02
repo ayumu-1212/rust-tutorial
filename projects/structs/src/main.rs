6a30407a945c28a7c101a27b008d0d2c313ae1a3
ayumu-1212 <ayumuabe1434@gmail.com>
2024-07-02 15:17:04 +0000

    add: tuple struct

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -1,26 +1,7 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let user1 = build_user(String::from("sample@example.com"), String::from("peko"));
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    println!("user1: {}", user1.active)
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 }
