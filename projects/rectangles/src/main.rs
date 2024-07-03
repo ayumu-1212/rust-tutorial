3c43cafc6448ab424e191de39227e24c2b7c202b
ayumu-1212 <ayumuabe1434@gmail.com>
2024-07-03 15:15:32 +0000

    update: Struct

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,14 +1,20 @@
-struct Rectangle(u32, u32);
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    let rectangle = Rectangle(30, 50);
+    let rect1 = Rectangle {
+        width: 50,
+        height: 30,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rectangle)
+        area(rect1)
     );
 }
 
 fn area(dimensions: Rectangle) -> u32 {
-    dimensions.0 * dimensions.1
+    dimensions.width * dimensions.height
 }
