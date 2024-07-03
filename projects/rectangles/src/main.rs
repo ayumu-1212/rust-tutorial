5d610f2904c90af91043750ec60b69296ccb5510
ayumu-1212 <ayumuabe1434@gmail.com>
2024-07-03 15:34:04 +0000

    add: output struct

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -14,10 +15,7 @@ fn main() {
         area(&rect1)
     );
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
+    println!("rect1 is {:#?}", rect1);
 }
 
 fn area(dimensions: &Rectangle) -> u32 {
